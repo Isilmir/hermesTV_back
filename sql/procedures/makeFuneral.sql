e35d12aab271f4e5cb8f28b35d7e3babc68fd49c
Isilmir <silar@list.ru>
2021-05-18 21:57:20 +0300

    добавил в процедуру похорон параметр expired

--- sql/procedures/makeFuneral.sql
+++ sql/procedures/makeFuneral.sql
@@ -14,14 +14,15 @@ GO
 --				2. Добавляет ачивку Смерть телу героя
 --              3. Переводит сданное тело в статус мертв
 
--- exec dbo.makeFuneral 43636,'player',43634,'player'
+-- exec dbo.makeFuneral 43636,'player',43634,'player',0
 -- =============================================
 CREATE PROCEDURE dbo.makeFuneral
 (
     @id_SUBJECT int, --id того кто сдает
 	@objectType_SUBJECT varchar(255), -- тип того кто сдает
 	@id_OBJECT int, --id того кого сдают
-	@objectType_OBJECT varchar(255) -- тип того кого сдают
+	@objectType_OBJECT varchar(255), -- тип того кого сдают
+	@expired bit = 0-- если похороны просрочены то герой не получает славы
 )
 AS
 BEGIN
@@ -83,7 +84,7 @@ BEGIN
 
 	--select @id_SUBJECT as [@id_SUBJECT],@objectType_SUBJECT as[@objectType_SUBJECT],@id_SUBJECT_p as [@id_SUBJECT_p],@objectType_SUBJECT_p as [@objectType_SUBJECT_p],@side_SUBJECT as[@side_SUBJECT],@id_OBJECT as[@id_OBJECT],@objectType_OBJECT as [@objectType_OBJECT],@side_OBJECT as [@side_OBJECT],@state_OBJECT as [@state_OBJECT]
 	
-	select @deedType = id, @deedHonor = defaultHonor,@deathCase = case when name='bodyally' then 1 when name='bodyenemy' then 2 else null end 
+	select @deedType = id, @deedHonor = case when @expired = 1 then 0 else defaultHonor end,@deathCase = case when name='bodyally' then 1 when name='bodyenemy' then 2 else null end 
 	from dbo.deedTypes
 	where name=case
 		when @objectType_OBJECT='player' then 'bodyhero'
